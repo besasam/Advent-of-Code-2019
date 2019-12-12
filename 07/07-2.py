712da87b2ce9db144d360329d0f030973d3c71dc
Terrapin <besasama@outlook.com>
2019-12-12 13:54:15 +0100

    Day 07 Part 2

--- 07/07-2.py
+++ 07/07-2.py
@@ -1,8 +1,5 @@
 import itertools
 
-lastOutput = 0
-
-
 def parseInstruction(instruction):
     instruction = str(instruction)
     while len(instruction) < 5:
@@ -11,16 +8,22 @@ def parseInstruction(instruction):
     return res
 
 
-class Thruster(object):
+class Amp(object):
 
-    def __init__(self, intcode=None, pointer=0, phase=None, input=None):
-        self.intcode = intcode
-        self.pointer = pointer
-        self.phase = phase
-        self.input = input
+    def __init__(self):
+        self.intcode = None
+        self.pointer = 0
+        self.phase = None
+        self.input = None
+        self.output = None
+        self.usedPhase = False
+        self.didOutput = False
         self.halt = False
+        self.isFirst = False
+        self.isLast = False
 
     def execute(self):
+        input = self.input if self.usedPhase else self.phase
         p = self.pointer
         instruction = parseInstruction(self.intcode[p])
         opcode = instruction['opcode']
@@ -46,64 +49,72 @@ class Thruster(object):
                 self.intcode[result] = 1
             else:
                 self.intcode[result] = 0
+            self.pointer += 4
+        elif opcode in range(3, 5):
+            if opcode == 3:
+                self.intcode[first] = input
+                if not self.usedPhase:
+                    self.usedPhase = True
+            elif opcode == 4:
+                self.output = self.intcode[first] if mode_first == 0 else first
+                self.didOutput = True
+            self.pointer += 2
+        elif opcode in range(5, 7):
+            if (opcode == 5 and (self.intcode[first] if mode_first == 0 else first) != 0) or (
+                    opcode == 6 and (self.intcode[first] if mode_first == 0 else first) == 0):
+                second = self.intcode[p + 2]
+                self.pointer = self.intcode[second] if mode_second == 0 else second
+            else:
+                self.pointer += 3
+        return self.output
 
-
-def eexecute(inputlist, input, pointer=0, secondinput=None, useSecondInput=False):
-    global lastOutput
-    instruction = parseInstruction(inputlist[pointer])
-    opcode = instruction['opcode']
-    mode_first = instruction['mode_first']
-    mode_second = instruction['mode_second']
-    if opcode == 99:
-        return inputlist
-    first = inputlist[pointer + 1]
-    if opcode in range(1, 3) or opcode in range(7, 9):
-        second = inputlist[pointer + 2]
-        result = inputlist[pointer + 3]
-        if opcode == 1:
-            inputlist[result] = (inputlist[first] if mode_first == 0 else first) + (
-                inputlist[second] if mode_second == 0 else second)
-        elif opcode == 2:
-            inputlist[result] = (inputlist[first] if mode_first == 0 else first) * (
-                inputlist[second] if mode_second == 0 else second)
-        elif (opcode == 7 and (inputlist[first] if mode_first == 0 else first) < (
-        inputlist[second] if mode_second == 0 else second)) or (
-                opcode == 8 and (inputlist[first] if mode_first == 0 else first) == (
-        inputlist[second] if mode_second == 0 else second)):
-            inputlist[result] = 1
-        else:
-            inputlist[result] = 0
-        return execute(inputlist, input, pointer + 4, secondinput, True)
-    elif opcode in range(3, 5):
-        if opcode == 3:
-            inputlist[first] = input if not useSecondInput else secondinput
-        elif opcode == 4:
-            output = inputlist[first] if mode_first == 0 else first
-            lastOutput = output
-            # print(output)
-        return execute(inputlist, input, pointer + 2, secondinput, True)
-    elif opcode in range(5, 7):
-        if (opcode == 5 and (inputlist[first] if mode_first == 0 else first) != 0) or (
-                opcode == 6 and (inputlist[first] if mode_first == 0 else first) == 0):
-            second = inputlist[pointer + 2]
-            return execute(inputlist, input, (inputlist[second] if mode_second == 0 else second), secondinput, True)
-        return execute(inputlist, input, pointer + 3, secondinput, True)
-    else:
-        print("Something went wrong")
-        return
+    def executeUntilOutput(self):
+        while not self.didOutput and not self.halt:
+            self.execute()
+        self.didOutput = False
+        return self.output
 
 
 inputlist = [int(x) for x in open('input.txt').read().split(',')]
-sequences = list(itertools.permutations([0, 1, 2, 3, 4]))
+sequences = list(itertools.permutations([x for x in range(5,10)]))
+
+amps = []
+for i in range(0,5):
+    amps.append(Amp())
+amps[0].isFirst = True
+amps[4].isLast = True
+
+
+def initializeAmps(amps, intcode, phases):
+    amps[0].input = 0
+    for i in range(0,5):
+        amps[i].intcode = intcode.copy()
+        amps[i].phase = phases[i]
+        amps[i].halt = False
+        amps[i].pointer = 0
+        amps[i].usedPhase = False
+
+
+def runAmps(amps):
+    output = None
+    i = 0
+    while not amps[4].halt:
+        output = amps[i].executeUntilOutput()
+        if i == 4:
+            amps[0].input = output
+            i = 0
+        else:
+            amps[i + 1].input = output
+            i += 1
+    return output
+
+
 highestOutput = 0
 
 for sequence in sequences:
-    input = 0
-    for i in range(0, 5):
-        program = inputlist.copy()
-        execute(program, sequence[i], 0, input)
-        input = lastOutput
-    if lastOutput > highestOutput:
-        highestOutput = lastOutput
+    initializeAmps(amps, inputlist, sequence)
+    output = runAmps(amps)
+    if output > highestOutput:
+        highestOutput = output
 
 print(highestOutput)
